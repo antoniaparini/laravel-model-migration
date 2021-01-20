3a1bf6b6f689847011ebedd893f70d977e526757
Antonia P <direiellie@gmail.com>
2021-01-20 17:26:16 +0100

    Update 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,9 @@
 @extends('layouts.main')
+@section('content')
+<h1>The Car of your Dreams</h1>
+
+<h2>Our Cars</h2>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus distinctio sequi expedita perferendis soluta, autem cupiditate minima suscipit nemo, commodi fuga praesentium, nihil perspiciatis eum! Sequi delectus consectetur sint alias?</p>
+    
+@endsection
